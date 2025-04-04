b16d5b45210e0faa18a23c625f5c422b6f549697
AntoxaPriz <prizbarisbritva@gmail.com>
2025-04-04 22:20:54 +0300

    Чистка
    Почистил структуру от старого проекта

--- includes/footer.php
+++
@@ -1,6 +0,0 @@
-<footer>
-    <p>© 2025 Юридическая клиника</p>
-    <p><a href="privacy.php">Политика конфиденциальности</a> | <a href="partners.php">Для партнёров</a></p>
-</footer>
-</body>
-</html>
